b203577235c9d8ce5e7d978e4018ca1ecf31b1f8
ob-medical-01 <chamroeunchet01@gmail.com>
2026-05-28 12:38:48 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -200,6 +200,8 @@ const USERS = [
    {username: "Longpheng", password: "pheng168", startDate: "2026-26-05", durationDays: 367},
   {username: "Nhemchenda", password: "Chendaa15@", startDate: "2026-28-05", durationDays: 367},
    {username: "Chansey12@V", password: "Chan12&$", startDate: "2026-28-05", durationDays: 367},
+   {username: "ChincheaCheatta", password: "Cheatta78", startDate: "2026-28-05", durationDays: 367},
+{username: "KongSovathpanha", password: "Panha", startDate: "2026-28-05", durationDays: 367},
   
   //------laboratory accounts-----
   { username: "Test", password: "test@2026", startDate: "2026-07-04", durationDays: 1 },
